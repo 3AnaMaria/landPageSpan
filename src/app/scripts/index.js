12cd83f9bd1328ba053281896080bb60375e1368
AnaMaria <anamahj@gmail.com>
2023-07-10 10:35:38 -0500

    Upsate Js buttons

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -40,6 +40,15 @@ company.addEventListener('click', ()=>{
  const menu = d.getElementById('menu')
  const menu_close_icon = d.getElementById('menu_close_icon')
  const menu_icon = d.getElementById('menu_icon')
+ const features_mobile = d.getElementById('features_mobile')
+ const company_mobile = d.getElementById('company_mobile')
+ const down_arrow_features_mobile = d.getElementById ('down_arrow_features_mobile')
+ const up_arrow_features_mobile = d.getElementById ('up_arrow_features_mobile')
+ const down_arrow_company_mobile = d.getElementById ('down_arrow_company_mobile')
+ const up_arrow_company_mobile = d.getElementById ('up_arrow_company_mobile')
+ const menu_features_mobile = d.querySelector('.menu_features_mobile')
+ const menu_company_mobile = d.querySelector('.menu_company_mobile')
+ 
 
  menu.addEventListener('click', ()=>{
     if (mobile_menu.classList.contains('hidden')) {
@@ -53,8 +62,25 @@ company.addEventListener('click', ()=>{
       }
  })
 
-const toggleMenu = (clickedEl, openingEl) => {
-  clickedEl.addEventListener('click', () => {
-      openingEl.classList.toggle('inactive');
+features_mobile.addEventListener('click', ()=>{
+  if (menu_features_mobile.classList.contains('hidden')) {
+      down_arrow_features_mobile.classList.add('hidden');
+      up_arrow_features_mobile.classList.remove('hidden')
+      menu_features_mobile.classList.remove('hidden');
+    } else {
+      down_arrow_features_mobile.classList.remove('hidden');
+      up_arrow_features_mobile.classList.add('hidden')
+      menu_features_mobile.classList.add('hidden');
+    }
 })
-}
+company_mobile.addEventListener('click', ()=>{
+  if (menu_company_mobile.classList.contains('hidden')) {
+      down_arrow_company_mobile.classList.add('hidden');
+      up_arrow_company_mobile.classList.remove('hidden')
+      menu_company_mobile.classList.remove('hidden');
+    } else {
+      down_arrow_company_mobile.classList.remove('hidden');
+      up_arrow_company_mobile.classList.add('hidden')
+      menu_company_mobile.classList.add('hidden');
+    }
+})
